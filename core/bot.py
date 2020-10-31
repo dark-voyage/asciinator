28ddf344b7f7de03e9237c08c35b592adf19afc8
Geno Ferollo <genemators@gmail.com>
2020-11-01 03:13:15 +0500

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -1,16 +1,20 @@
 try:
+    from os import environ
     from pyrogram import Client, filters
 except ImportError as Error:
     print("Error occurred while importing:", Error)
 
 app = Client(
+    api_id=environ.get("API_ID"),
+    api_hash=environ.get("API_HASH"),
     session_name="main",
     app_version="0.0.3",
-    device_model="OSS Linux by Genemator",
-    system_version="Geolinda V3.1.3 (Sara Edition)",
+    device_model="Geolinda V3.1.3 (Sara Edition)",
+    system_version="OSS Linux by Genemator",
     lang_code="en",
+    phone_number=environ.get("PHONE"),
+    password=environ.get("PASSWORD"),
     parse_mode="html",
-    config_file="./config.ini",
     hide_password=True,
     plugins=dict(root="plugins")
 )
