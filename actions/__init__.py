27f9ea8517358a6af62ceee0c6fa6f12571ac007
Geno Ferollo <genemators@gmail.com>
2020-11-01 03:56:09 +0500

    launcher preview

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,1 +1,13 @@
-from .launcher import launcher
+from core import app
+from time import sleep
+from pyrogram import errors
+
+
+def launcher():
+    try:
+        app.run()
+    except errors.FloodWait as e:
+        sleep(e.x)
+    except errors.RPCError as e:
+        exit(1)
+
